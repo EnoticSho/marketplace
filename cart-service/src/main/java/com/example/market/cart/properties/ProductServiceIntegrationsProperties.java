e1d36ad1d935ce4b8bc1b25f4af5f5ef81136470
Sergey Turpakov <EnoticSho@gmail.com>
2023-03-22 08:47:40 +0300

    updated: property link

--- cart-service/src/main/java/com/example/market/cart/properties/ProductServiceIntegrationsProperties.java
+++ cart-service/src/main/java/com/example/market/cart/properties/ProductServiceIntegrationsProperties.java
@@ -3,18 +3,12 @@ package com.example.market.cart.properties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "integrations.products-service")
+@ConfigurationProperties(prefix = "integrations.product-service")
+
 @Data
 public class ProductServiceIntegrationsProperties {
     private String url;
-    private Integer connectTimeout;
     private Integer readTimeout;
     private Integer writeTimeout;
-
-    public ProductServiceIntegrationsProperties(String url, Integer connectTimeout, Integer readTimeout, Integer writeTimeout) {
-        this.url = url;
-        this.connectTimeout = connectTimeout;
-        this.readTimeout = readTimeout;
-        this.writeTimeout = writeTimeout;
-    }
+    private Integer connectTimeout;
 }
